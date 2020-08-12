fd8caece0a57b5f294eaf57d0606b856e6cd71eb
molysama <molysama.i@gmail.com>
2020-08-12 16:25:02 +0800

    webpack添加开发环境支持

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const JavascriptObfuscator = require("webpack-obfuscator")
 const AutoProWebpackPlugin = require('@auto.pro/webpack-plugin')
 
+
 const dictionary = []
 for (let i = 1024; i < 2048; i++) {
     dictionary.push(
@@ -13,7 +14,14 @@ for (let i = 1024; i < 2048; i++) {
     )
 }
 
-module.exports = {
+const compilePlugin = new AutoProWebpackPlugin({
+    ui: ["app", "login"],
+    // entry: {
+    //     key: ''
+    // }
+})
+
+const config = {
     entry: {
         app: "./src/index.js",
         login: "./src/login.js"
@@ -25,25 +33,6 @@ module.exports = {
         // libraryTarget: "commonjs2"
     },
     target: "node",
-
-    mode: "production",
-    plugins: [
-        new CleanWebpackPlugin(),
-        new JavascriptObfuscator({
-            compact: true,
-            identifierNamesGenerator: "dictionary",
-            identifiersDictionary: dictionary,
-            // 生成的代码环境，可选browser、browser-no-eval、node
-            target: "browser-no-eval",
-            // 混淆对象键名
-            transformObjectKeys: false,
-            // 将字符串明文转义为Unicode，会大大增加体积，还原也比较容易，建议只对小文件使用
-            unicodeEscapeSequence: true
-        }),
-        new AutoProWebpackPlugin({
-            ui: ['app', 'login']
-        })
-    ],
     module: {
         rules: [
             {
@@ -74,4 +63,32 @@ module.exports = {
             "@": path.resolve(__dirname, "src")
         }
     }
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.plugins = [
+            new CleanWebpackPlugin(),
+            compilePlugin
+        ]
+        config.devtool = 'source-map'
+    } else {
+        config.plugins = [
+            new CleanWebpackPlugin(),
+            new JavascriptObfuscator({
+                compact: true,
+                identifierNamesGenerator: "dictionary",
+                identifiersDictionary: dictionary,
+                // 生成的代码环境，可选browser、browser-no-eval、node
+                target: "browser-no-eval",
+                // 混淆对象键名
+                transformObjectKeys: false,
+                // 将字符串明文转义为Unicode，会大大增加体积，还原也比较容易，建议只对小文件使用
+                unicodeEscapeSequence: false
+            }),
+            compilePlugin
+        ]
+    }
+
+    return config
 }
